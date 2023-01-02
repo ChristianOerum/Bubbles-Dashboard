5fc514918ded8a454ae51bab5f76641d4d531e89
Christian Ørum <chri@bubbles.dk>
2023-01-02 15:12:26 +0100

    import af projekt fra anden fil

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Dashpage>
+  </Dashpage>
+</template>
+
+<script>
+//import af tailwind CSS
+import './assets/tailwind.css'
+//import af dash_page komponent
+import Dashpage from "./components/Dash_page.vue";
+import { defineComponent } from 'vue';
+
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Dashpage
+  },
+  data() {
+    return {}
+  },
+
+  methods: {}
+});
+</script>
---
+++ src/components/Dash_page.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="bg-slate-700 w-100 h-screen flex justify-center items-center p-10">
+        <div class="w-full h-full grid grid-rows-3 grid-cols-5">
+
+          <div class="bg-slate-800 rounded-xl col-span-1 row-span-1 m-2 flex flex-col justify-center items-center">
+            <p class="text-white text-xl">Lable</p>
+            <h1 class="text-white text-5xl">232</h1>
+          </div>
+
+          <div class="bg-slate-800 rounded-xl col-span-1 row-span-1 m-2 flex flex-col justify-center items-center">
+            <p class="text-white text-xl">Lable</p>
+            <h1 class="text-white text-5xl">34%</h1>
+          </div>
+
+          <div class="bg-slate-800 rounded-xl col-span-1 row-span-1 m-2 flex flex-col justify-center items-center">
+            <p class="text-white text-xl">Lable</p>
+            <h1 class="text-white text-5xl">...</h1>
+          </div>
+
+          <div class="bg-slate-800 rounded-xl col-span-1 row-span-1 m-2 flex flex-col justify-center items-center">
+            <p class="text-white text-xl">Lable</p>
+            <h1 class="text-white text-5xl">...</h1>
+          </div>
+          <div class="bg-slate-800 rounded-xl col-span-1 row-span-1 m-2">
+
+          </div>
+          <div class="bg-slate-800 rounded-xl col-span-1 row-span-1 m-2">
+
+          </div>
+          <div class="bg-slate-800 rounded-xl col-span-1 row-span-1 m-2">
+
+          </div>
+          <div class="bg-slate-800 rounded-xl col-span-1 row-span-1 m-2">
+
+          </div>
+          <div class="bg-slate-800 rounded-xl col-span-1 row-span-1 m-2">
+
+          </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
