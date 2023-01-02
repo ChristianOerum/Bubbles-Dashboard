d7c09d4cbf8d7c00c732a0e5b01c3266ecb8f49a
Christian Ørum <chri@bubbles.dk>
2023-01-02 15:17:42 +0100

    Update Dash_page.vue

--- src/components/Dash_page.vue
+++ src/components/Dash_page.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="bg-slate-700 w-100 h-screen flex justify-center items-center p-10">
-        <div class="w-full h-full grid grid-rows-3 grid-cols-5">
+        <div class="w-full h-full grid grid-rows-4 grid-cols-7">
 
           <div class="bg-slate-800 rounded-xl col-span-1 row-span-1 m-2 flex flex-col justify-center items-center">
             <p class="text-white text-xl">Lable</p>
