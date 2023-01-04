129c34bfa91fb7dc858b49a16a69ee3e666d4dcd
Christian Ørum <chri@bubbles.dk>
2023-01-04 19:13:42 +0100

    Update Dash_page.vue

--- src/components/Dash_page.vue
+++ src/components/Dash_page.vue
@@ -46,10 +46,6 @@
 
         </div>
 
-        <div class="bg-red h-{30}"> 
-          <p id="test" class="text-slate-500 text-md">{{ip()}}</p>
-        </div>
-
     </div>
 </template>
 
@@ -59,49 +55,6 @@ export default {
   name: 'App',
   components: {
   },
-  methods: {
-    ip: function(){
-
-
-
-      function getIPs(callback){
-    var ips = [];
-
-    var RTCPeerConnection = window.RTCPeerConnection ||
-        window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
-
-    var pc = new RTCPeerConnection({
-        // Don't specify any stun/turn servers, otherwise you will
-        // also find your public IP addresses.
-        iceServers: []
-    });
-    // Add a media line, this is needed to activate candidate gathering.
-    pc.createDataChannel('');
-    
-    // onicecandidate is triggered whenever a candidate has been found.
-    pc.onicecandidate = function(e) {
-        if (!e.candidate) { // Candidate gathering completed.
-            pc.close();
-            callback(ips);
-            return;
-        }
-        var ip = /^candidate:.+ (\S+) \d+ typ/.exec(e.candidate.candidate)[1];
-        if (ips.indexOf(ip) == -1) // avoid duplicate entries (tcp/udp)
-            ips.push(ip);
-    };
-    pc.createOffer(function(sdp) {
-        pc.setLocalDescription(sdp);
-    }, function onerror() {});
-}
-
-  getIPs(function(ips){
-      //console.log('IPs: ', ips);
-      document.getElementById("test").innerHTML = ips;
-});
-
-
-
-    }
-  }
+  methods: {}
 }
 </script>
