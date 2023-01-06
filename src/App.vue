036c78ee980f2315061f7bd59f0576b14c9644fc
Christian Ørum <chri@bubbles.dk>
2023-01-06 15:20:47 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,15 @@
 
   <button class="bg-[#43474D] right-5 bottom-5 rounded-md w-14 h-14 absolute flex flex-col justify-center items-center" @click="playSong()">
     <svgCreater class="h-10 w-10" v-model:name=musicOnLable></svgCreater>
+
     <audio v-on:ended=songEnded() id="audio-player">
         <source src="https://github.com/ChristianOerum/Bubbles-Dashboard/blob/main/music/Just%20the%20two%20of%20us.mp3?raw=true" type="audio/mp3">
     </audio>
+
+    <audio id="alarm-audio-player">
+        <source src="https://github.com/ChristianOerum/Bubbles-Dashboard/blob/main/music/Alarm%20Siren%20Sound%20Fx.mp3?raw=true" type="audio/mp3">
+    </audio>
+
   </button>
 
 </template>
@@ -58,10 +64,15 @@ export default defineComponent({
   },
   mounted() {
     window.addEventListener("keypress", function(e) {
-      if (String.fromCharCode(e.keyCode) == "k") {
-        console.log("ja tjark")
+      if (String.fromCharCode(e.keyCode) == "s") {
+        console.log("Run Sales alarm!")
+        
+        let audio = document.getElementById("alarm-audio-player");
+        audio.play()
+        audio.volume = 0.6
       }
       else {
+        //logger alle characterer som ikke er "s"
       console.log(String.fromCharCode(e.keyCode));
     }
 
